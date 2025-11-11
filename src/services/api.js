46553b90ef66d65d02718d0aab8483e966b1964f
Aman <amandeep3771.beai23@chitkara.edu.in>
2025-11-11 23:21:02 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -8,17 +8,3 @@ const api = axios.create({
 });
 
 export default api;
-
-import api from './api';
-
-export const login = async (credentials) => {
-  const response = await api.post('/auth/login', credentials);
-  return response.data;
-};
-
-export const signup = async (userData) => {
-  const response = await api.post('/auth/signup', userData);
-  return response.data;
-};
-
-// Add other auth-related methods as needed
